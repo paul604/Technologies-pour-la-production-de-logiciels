c9e57077c7419ad63ca492f8934d62b4eda296c5
paul604 <paulorhon@live.fr>
2017-03-09 12:08:07 +0100

    fix csv2db_installation (mising commit)

--- src/database/admin/update_from_csv.py
+++ src/database/admin/update_from_csv.py
@@ -1,13 +1,14 @@
 
 import csv
+import sqlite3
 
 def csv2db_installation():
     '''
     update de la table installations de la db en utilisant le fichier csv.
     '''
 
-    read = csv.DictReader(file)
     file = open('data/csv/installations.csv','r')
+    read = csv.DictReader(file)
     tab_instalation = []
     tab_addr = []
     for row in read:
@@ -24,10 +25,34 @@ def csv2db_installation():
             ,row['Code postal']
             ,row['Nom de la commune']
         ))
-    conn = sqlite3.connect('data/database/db.db')
-    cursor = conn.cursor()
 
-    cursor.executemany('INSERT INTO installation VALUES (?,?)', tab_instalation)
-    cursor.executemany('INSERT INTO adresse VALUES (?,?,?,?)', tab_addr)
+    try:
+        conn = sqlite3.connect('data/database/db.db')
+        cursor = conn.cursor()
 
-up_installations()
+        cursor.executemany('INSERT INTO installation VALUES (?,?)', tab_instalation)
+        cursor.executemany('INSERT INTO adresse VALUES (?,?,?,?)', tab_addr)
+
+        conn.commit()
+
+    except Exception as e:
+    	print (type(e))
+    	conn.rollback()
+    finally:
+    	conn.close()
+
+
+#test
+
+csv2db_installation()
+
+try:
+    conn2 = sqlite3.connect('data/database/db.db')
+    cursor2 = conn2.cursor()
+    for row in cursor2.execute('SELECT * FROM installation',):
+        print (row)
+
+except Exception as e:
+    raise
+finally:
+    conn2.close()
