cb2da15deb9d8972210dcfbc9b1f4bf624a834a6
ReedShalazi <rems.v1@gmail.com>
2017-03-16 10:55:09 +0100

    get_data() prend maintenant le path en paramètre du dossier devant contenir les données

--- src/database/admin/get_data.py
+++ src/database/admin/get_data.py
@@ -9,26 +9,23 @@ Fournit un ensemble de fonction pour :
 import os, urllib.request
 import csv
 
-def dl_data(override = False):
+def dl_data(where_to_put_data, override = False):
 	'''
 	Le paramètre override permet d'activer ou non l'écrasement des CSV.
 	Activer pour les télécharger à nouveau les CVS même si les fichiers existent.
 	'''
 
 	# on crée le dossier s'il n'existe pas
-	if not os.path.exists('data'):
-		os.mkdir('data');
+	if not os.path.exists(where_to_put_data+'/data'):
+		os.mkdir(where_to_put_data+'/data');
 
-	if not os.path.exists('data/csv'):
-		os.mkdir('data/csv');
+	if not os.path.exists(where_to_put_data+'/data/csv'):
+		os.mkdir(where_to_put_data+'/data/csv');
 
 	# on y télécharge les CSV
-	if override or not os.path.isfile('data/csv/installations.csv'):
-		urllib.request.urlretrieve('http://data.paysdelaloire.fr/api/publication/23440003400026_J335/installations_table/content/?format=csv', 'data/csv/installations.csv')
-	if override or not os.path.isfile('data/csv/equipements.csv'):
-		urllib.request.urlretrieve('http://data.paysdelaloire.fr/fileadmin/data/datastore/rpdl/sport/23440003400026_J336/equipements.csv', 'data/csv/equipements.csv')
-	if override or not os.path.isfile('data/csv/activites.csv'):
-		urllib.request.urlretrieve('http://data.paysdelaloire.fr/fileadmin/data/datastore/pdl/PLUS15000/J334_equipements_activites.csv', 'data/csv/activites.csv')
-
-
-dl_data()
+	if override or not os.path.isfile(where_to_put_data+'/data/csv/installations.csv'):
+		urllib.request.urlretrieve('http://data.paysdelaloire.fr/api/publication/23440003400026_J335/installations_table/content/?format=csv', where_to_put_data+'/data/csv/installations.csv')
+	if override or not os.path.isfile(where_to_put_data+'/data/csv/equipements.csv'):
+		urllib.request.urlretrieve('http://data.paysdelaloire.fr/fileadmin/data/datastore/rpdl/sport/23440003400026_J336/equipements.csv', where_to_put_data+'/data/csv/equipements.csv')
+	if override or not os.path.isfile(where_to_put_data+'/data/csv/activites.csv'):
+		urllib.request.urlretrieve('http://data.paysdelaloire.fr/fileadmin/data/datastore/pdl/PLUS15000/J334_equipements_activites.csv', where_to_put_data+'/data/csv/activites.csv')
--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
+#!/usr/bin/env python3.4
+
 
 '''
-Appelle l'ensemble 
+Contient l'exécution séquentielle des différentes fonctions.
+Permet de tester le bon enchaînement des différents appels.
 '''
+
+
+# évite de générer des dossier __pycache__ indésirables dans l'arborescence du projet
+import sys
+sys.dont_write_bytecode = True
+
+
+# on récupère le path de la racine du projet en sachant que le main est toujours placé à la racine du projet sous src
+# <project_root>/src/main.py
+import os
+project_root = os.path.abspath(os.path.dirname(os.path.abspath(__file__))+'/..')
+
+
+from database.admin.get_data import dl_data
+
+
+
+dl_data(project_root)
+
