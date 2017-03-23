f6d8dd68952c1c437da530be0aaa0d9636383ad8
ReedShalazi <rems.v1@gmail.com>
2017-03-23 10:35:28 +0100

    structure projet retravaillée pour intégrer l'api rest

---
+++ src/server/rest/api/endpoint.py
@@ -0,0 +1,15 @@
+
+'''
+Met à disposition un service web de récupération de de données en JSON à l'aide de la librairie bottle.
+'''
+
+
+def launch_rest_api_service():
+	run(host='localhost', port=1234, debug=True) # ne pas utiliser 80 ou 8080 (déjà utilisés)
+
+
+# méthode de test
+@route('/')
+def hello():
+    return "Hello World!"
+
