4693ae292d1398ab1296c18d6313adeb4977e002
ReedShalazi <rems.v1@gmail.com>
2017-03-28 18:57:26 +0200

    removed useless wrapper functions

--- src/server/packages/rest/api/endpoint.py
+++ src/server/packages/rest/api/endpoint.py
@@ -1,4 +1,10 @@
 from ..lib.bottle import run
+
+
+
+
+# /!\ ne pas retirer /!\
+# imports servant à charger les routes
 from .routes.installation import i_route
 from .routes.equipement import e_route
 from .routes.activite import a_route
@@ -22,31 +28,3 @@ def launch_rest_api_service(host='localhost', port=1234, debug=False):
 	Permet le lancement du serveur bottle
 	'''
 	run(host=host, port=port, debug=debug)
-
-
-
-# -------------- méthodes wrappers : définition des routes dans leurs fichiers dédiés respectifs
-
-
-def get_villes():
-	return v_route()
-
-
-def get_codes_postaux():
-	return cp_route()
-
-
-def get_cp_and_v():
-	return cp_et_v_route()
-
-
-def get_i():
-	return i_route()
-
-
-def get_e():
-	return e_route()
-
-
-def get_a():
-	return a_route()
