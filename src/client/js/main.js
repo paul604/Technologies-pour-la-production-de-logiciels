8e2c39872c7d0d90f91d9578252c752784ed78a9
paul604 <paulorhon@live.fr>
2017-03-30 11:20:05 +0200

    autocomplete for ville & activite, bug CORS

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,82 @@
-$(function(){
-  
+////////////////////////////////////////// s'exécute après le chargement du HTML
+$(document).ready(function(){
+
+    autocomplete();
+
 });
+//////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////// gère l'autocomplet
+function autocomplete(){
+
+    //ville
+    var champ_ville = $("#ville");
+
+    console.log(champ_ville.val());
+    champ_ville.autocomplete({
+        minLength: 1,
+        source:function(ville, response){
+          getVille(ville.term, response);
+        }
+    });
+
+
+    //activite
+    var champ_activite = $("#activite");
+
+    champ_activite.autocomplete({
+        minLength: 1,
+        source:function(activite, response){
+          getActivite(activite.term, response);
+        }
+    });
+
+}
+//////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////// perme de get les vill matchant avec `ville`
+function getVille(ville, response){
+  var outData='suggestions='+ville;
+  $.ajax({
+      url : 'http://localhost:1234/data/villes',
+      type : 'get',
+      dataType : 'json',
+      data : outData,
+      success : function(out, statut){
+        var tab = [];
+        $.each(out, function (i, val) {
+          tab[tab.length]=val;
+        });
+        response(tab);
+      }
+  });
+};
+//////////////////////////////////////////
+
+
+
+
+function getActivite(activite, response){
+    var outData='suggestions='+activite;
+    $.ajax({
+        url : 'http://localhost:1234/data/activites',
+        type : 'get',
+        dataType : 'json',
+        data : outData,
+        success : function(out, statut){
+          var tab = [];
+          $.each(out, function (i, val) {
+            tab[tab.length]=val;
+          });
+          response(tab);
+        }
+    });
+}
